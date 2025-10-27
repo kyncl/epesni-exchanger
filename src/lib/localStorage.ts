073ac386f6c7d86107a6e67d4d937b567a64dab4
kyncl <145004616+kyncl@users.noreply.github.com>
2025-10-27 15:52:53 +0100

    I'm dump

--- src/lib/localStorage.ts
+++ src/lib/localStorage.ts
@@ -3,12 +3,8 @@ import type { currencyStamp } from "./currency"
 export const getSavedRates = (): currencyStamp | null => {
     const savedStampStr = localStorage.getItem("savedCunrrencyStamp");
     const savedStamp = JSON.parse(savedStampStr ?? "") as currencyStamp;
-    if (
-        savedStamp &&
-        typeof savedStamp === "object"
-    ) {
+    if (typeof savedStamp === "object")
         return savedStamp
-    }
 
     return null;
 }
